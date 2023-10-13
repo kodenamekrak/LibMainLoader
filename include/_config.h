1863df79a289cfde73d5808bd301a1b4b4b5c711
Sc2ad <adamznow@gmail.com>
2023-10-12 18:55:49 -0700

    Add NO_EXCESS_LOGGING macro

--- include/_config.h
+++ include/_config.h
@@ -2,3 +2,9 @@
 
 #define MAIN_EXPORT __attribute__((visibility("default")))
 #define MAIN_LOCAL __attribute__((visibility("hidden")))
+
+// Helper defines for reducing codesize
+#ifdef NO_EXCESS_LOGGING
+#define NO_SPECIFIC_FAIL_LOGS
+#define NO_STAT_DUMPS
+#endif
